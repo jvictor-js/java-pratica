8eeaf0e8f23c322ebdc7b288b0c2bbd498216682
João Victor Almeida <144543445+jvictor-js@users.noreply.github.com>
2025-09-01 15:01:13 -0300

    Novos códigos usando a estrutura de repetição for.

---
+++ src/aulajava/NumerosPares.java
@@ -0,0 +1,16 @@
+package aulajava;
+
+public class NumerosPares {
+
+	public static void main(String[] args) {
+		
+		
+		for (int i = 0; i <= 50; i++) {
+			if (i % 2 == 0) {
+				System.out.println(i);
+			}
+		}
+
+	}
+
+}
