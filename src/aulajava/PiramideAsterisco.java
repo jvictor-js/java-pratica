00e0698bd4e389a114b84d11632dcbdc118d80bb
João Victor Almeida <144543445+jvictor-js@users.noreply.github.com>
2025-09-02 14:19:30 -0300

    Add files via upload

---
+++ src/aulajava/PiramideAsterisco.java
@@ -0,0 +1,20 @@
+package aulajava;
+
+public class PiramideAsterisco {
+
+	public static void main(String[] args) {
+		int n = 5;
+		for (int i = 0; i <= n; i++) {
+			for (int j = 0; j <= n - i; j++) {
+				System.out.print(" ");
+			}
+			for (int k = 1; k <= 2 * i - 1; k++) {
+	            System.out.print("*");
+	        }
+			System.out.println();
+
+		}
+
+	}
+
+}
